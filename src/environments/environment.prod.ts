38f5328a82a941de00cca43b627cf1d52dc80df9
Joe Sandoval <sandoval.jhosep.110305@gmail.com>
2025-02-11 11:41:05 -0500

    Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: "production",
   environmentName: "production",
   backEnd: {
-    baseUrl:  "https://backend-korx-team.vercel.app",
+    baseUrl:  "https://backend-korx.vercel.app",
   },
   frontEnd: {
     baseUrl: "https://korxteam.netlify.app",
@@ -13,9 +13,9 @@ export const environment = {
 // Pruductions
 // Local: 
 // Backend  http://localhost:4000
-// baseUrl: "https://backend-korx-team.vercel.app",
+// baseUrl: "https://backend-korx.vercel.app",
 
 // FrontEnd
 // Local: http://localhost:3000
-// baseUrl: "https://korxteam.netlify.app",
+// baseUrl: "https://korxteam.netlify.app"
 
