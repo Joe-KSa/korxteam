937e602825dc2ab4ac5fed58306d6386aa9d8efd
Joe Sandoval <sandoval.jhosep.110305@gmail.com>
2025-02-11 01:33:09 -0500

    Fixed enviorenment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: "production",
   environmentName: "production",
   backEnd: {
-    baseUrl:  "http://localhost:4000",
+    baseUrl:  "https://backend-korx.vercel.app",
   },
   frontEnd: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://korxteam.netlify.app",
   },
 };
 
